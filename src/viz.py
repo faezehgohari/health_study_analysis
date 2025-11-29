f71da3ee4ca4b56fd95f70a790279a4fbf4883f1
faezehgohari <faeze.gohari68@gmail.com>
2025-11-29 20:34:59 +0100

    Comparison between real and simulated disease proportions

--- src/viz.py
+++ src/viz.py
@@ -39,3 +39,19 @@ def bar_percentage_smokers(df):
     ax.set_ylabel("Rökstatus")
     ax.grid(axis='y', linestyle='--' , alpha=0.4)
     return ax
+
+def bar_real_vs_simulated(df , values):
+    '''
+    Comparison between real and simulated disease proportions graph
+    '''
+    labels = ['Verklig', 'Simulerad']
+    fig, ax = plt.subplots(figsize=(8,5))
+    ax.bar(labels, values, color=['#4C72B0', '#76B7B2'], edgecolor='black', linewidth=1)
+    ax.set_title("Jämförelse mellan verklig och simulerad sjukdomsandel")
+    #ax.set_xlabel("Procent (%)")
+    ax.set_ylabel("Andel personer med sjukdom (%)")
+    ax.set_ylim(0, max(values) + 1)
+    ax.grid(axis='y', linestyle='--', alpha=0.4)
+    for i, v in enumerate(values):
+        ax.text(i, v+0.3, f"{v:.2f}%", ha='center', fontsize=11)
+    return ax
